56754848500bae345e8df4f1696ca36204ea4776
Fernando Pérez Vigo <fpvigo@gmail.com>
2025-01-28 18:57:58 +0100

    Create chat.js

---
+++ chat.js
@@ -0,0 +1,6 @@
+<script 
+src="https://cdn.cai.tools.sap/webclient/bootstrap.js" 
+data-channel-id="74c4fddb-edfb-4e7e-84b7-e37479256bfe" 
+data-token="1c53b6f97ea9691ee69c6abf14925dde" 
+data-expander-type="CAI" 
+data-expander-preferences="JTdCJTIyZXhwYW5kZXJMb2dvJTIyJTNBJTIyaHR0cHMlM0ElMkYlMkZjZG4uY2FpLnRvb2xzLnNhcCUyRndlYmNoYXQlMkZ3ZWJjaGF0LWxvZ28uc3ZnJTIyJTJDJTIyZXhwYW5kZXJUaXRsZSUyMiUzQSUyMkNsaWNrJTIwb24lMjBtZSElMjIlMkMlMjJvbmJvYXJkaW5nTWVzc2FnZSUyMiUzQSUyMkNoYXQlMjB3aXRoJTIwbWUhJTIyJTJDJTIydGhlbWUlMjIlM0ElMjJERUZBVUxUJTIyJTdE" id="cai-webclient-custom"> </script>
